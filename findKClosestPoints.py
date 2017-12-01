7a01582795e26a4dd3377a6339873f540902c7f4
youhusky <bozhaoli.usc@gmail.com>
2017-11-30 21:21:33 -0800

    commit

--- findKClosestPoints.py
+++ findKClosestPoints.py
@@ -4,18 +4,24 @@ class Point(object):
 		self.x = x
 		self.y = y
 
+# O (nlgk)
+# O(k)
 def findKClosestPoints(points, k):
 	pq = []
 	for point in points:
 		heapq.heappush(pq,(-point.x*point.x-point.y*point.y, point))
 		if len(pq) > k:
 			heapq.heappop(pq)
+	for i in pq:
+		print i[1].x, i[1].y
 	res = []
 	while pq:
 		res.append(heapq.heappop(pq)[1])
-	print res[0].x, res[0].y
+	# for i in res:
+	# 	print i.x, i.y
 	return res
 
 a = Point(1,1)
 b = Point(2,2)
-print findKClosestPoints([a,b],1)
+c = Point(3,3)
+print findKClosestPoints([a,b],2)
